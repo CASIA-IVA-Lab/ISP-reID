a1d60ae9c8e62fb0547f54ff5f7aa37b32945188
zhukuan96 <47589249+zhukuan96@users.noreply.github.com>
2020-08-23 19:08:09 +0800

    Add files via upload

---
+++ Visualize.sh
@@ -0,0 +1,2 @@
+
+python3 tools/visualize.py --num_part=7 --original_image_dir="/data/kzhu/DukeMTMC-reID/bounding_box_train" --pseudo_labels_dir="/data/kzhu/DukeMTMC-reID/train_pseudo_labels-ISP-7" --visualization_dir="/data/kzhu/DukeMTMC-reID/train_pseudo_labels-ISP-7-color" 
